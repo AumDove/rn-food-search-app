a4943ce04bbb65fc0fc19f06509e20a9b20f996b
aumdove <aumdove13@gmail.com>
2020-03-19 02:46:59 -0500

    added results screen to show name, photo, address, phone and website link for restaurant

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
 import yelp from '../api/yelp';
+import Hyperlink from 'react-native-hyperlink';
+import ResultsList from '../components/ResultsList';
 
 const ResultsShowScreen = ({ navigation }) => {
   const [result, setResult] = useState(null);
@@ -21,23 +23,59 @@ const ResultsShowScreen = ({ navigation }) => {
   console.log(id);
   return (
     <View>
-      <Text>{result.name}</Text>
+      <Text style={styles.titleStyle}>{result.name}</Text>
       <FlatList
         horizontal
         data={result.photos}
         keyExtractor={photo => photo}
         renderItem={({ item }) => {
-          return <Image style={styles.image} source={{ uri: item }} />;
+          return (
+            <View style={styles.imageContainer}>
+              <Image style={styles.image} source={{ uri: item }} />
+            </View>
+          );
         }}
       />
+      <View style={styles.infoContainer}>
+        <Text style={styles.address}>
+          {result.location.address1} {result.location.city},{' '}
+          {result.location.state}
+        </Text>
+        <Text style={styles.phone}>
+          Call now: {result.display_phone}
+        </Text>
+        <Hyperlink linkDefault={true}>
+          <Text style={{ fontSize: 15 }}>Website: {result.url}</Text>
+        </Hyperlink>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  titleStyle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    padding: 20
+  },
+  imageContainer: {
+    flex: 1
+  },
   image: {
     height: 100,
-    width: 100
+    width: 110
+  },
+  infoContainer: {
+    marginHorizontal: 15
+  },
+  address: {
+    fontSize: 18,
+    marginVertical: 10
+  },
+  phone: {
+    fontSize: 20,
+    marginVertical: 20
   }
 });
 
